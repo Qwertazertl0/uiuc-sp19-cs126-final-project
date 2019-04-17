b93ba564f9040d33cfb2892172eaff5e04e4129a
Maxwell Jong <mjong3@illinois.edu>
2019-04-16 20:05:23 -0500

    changed project from console to windows app and changed icon

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "../resource.h"
 
 //========================================================================
-int main( ){
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
   ofGLFWWindowSettings settings;
   settings.windowMode = OF_WINDOW;
   settings.setSize(1440, 960);
@@ -11,6 +11,11 @@ int main( ){
   settings.title = "DOT: A Visual Experience";
   settings.setPosition(glm::vec2(0, 38)); //title bar is 38 pixels
   ofCreateWindow(settings);
+
+  //Icon code from https://forum.openframeworks.cc/t/how-to-get-rid-of-console-and-set-proper-app-icon/24227
+  HWND hwnd = ofGetWin32Window();
+  HICON hMyIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+  SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hMyIcon);
 
   ofRunApp(new ofApp());
 }
